cf9ff866a5b2818c2fceadbe79557df0acc99f06
DeepakBandhu <78710411+DeepakBandhu@users.noreply.github.com>
2021-02-23 14:22:56 -0800

    Add files via upload

---
+++ Project2_functions/JS/main.js
@@ -0,0 +1,5 @@
+function My_First_Function() {
+    var str = "Cyan Text is Now on the Screen";
+    var result = str.fontcolor("cyan");
+    document.getElementById("Cyan_Text").innerHTML=result; 
+}
---
+++ Project2_functions/JS/plus_operator.js
@@ -0,0 +1,5 @@
+function myFunction() { //defining the function
+    var sentence = "I am learning"; // the senctence
+    sentence += " a lot from this book!";   // that will show up 
+    document.getElementById("Concatenate").innerHTML=sentence; // defining the id
+}
